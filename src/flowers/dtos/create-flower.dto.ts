49d359495f8d1fd6695e27d6336eab00325ace1e
Adrian Furman <adrian.furman@deployed.pl>
2022-05-27 12:43:15 +0200

    Add swagger documentation for dto

--- src/flowers/dtos/create-flower.dto.ts
+++ src/flowers/dtos/create-flower.dto.ts
@@ -1,26 +1,35 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 import { IsEnum, IsInt, IsString, IsOptional } from 'class-validator';
 
 import { SunDemand } from '../constants';
 
 class CreateFlowerDto {
+  @ApiProperty()
   @IsString()
   name: string;
 
+  @ApiProperty()
   @IsString()
   specie: string;
 
+  @ApiProperty()
   @IsInt()
   wateringIntervalInDays: number;
 
+  @ApiProperty({ enum: SunDemand })
   @IsEnum(SunDemand)
   sunDemand: SunDemand;
 
+  @ApiProperty()
   @IsInt()
   room: number;
 
+  @ApiProperty()
   @IsInt()
   sill: number;
 
+  @ApiProperty()
   @IsOptional()
   @IsString()
   sensorMacAddress: string;
